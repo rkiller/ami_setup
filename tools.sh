4730bf34329ceaacca25d12f77aa12759706946a
rkiller <rogerkiller@gmail.com>
2022-04-29 10:23:41 -0700

    Added general tools.sh

---
+++ tools.sh
@@ -0,0 +1,12 @@
+echo Install tools
+
+sudo yum update
+
+# From ossec.sh
+sudo yum install -y zlib-devel pcre2-devel make gcc zlib-devel pcre2-devel sqlite-devel openssl-devel libevent-devel
+
+# from imagemagick.sh
+sudo yum makecache
+sudo yum -y install libtool
+sudo yum -y install gcc-c++
+sudo yum -y groupinstall "Development Tools"
