a8bffffff52e55d09cc8e2b7bfdc88c9bfd6e660
rkiller <rogerkiller@gmail.com>
2022-04-30 18:52:29 -0700

    Platform changes pdftk-java

--- pdftk.sh
+++ pdftk.sh
@@ -1,3 +1,11 @@
-sudo wget -O /usr/lib64/libgcj.so.10 https://github.com/lob/lambda-pdftk-example/raw/master/bin/libgcj.so.10
-sudo wget -O /usr/bin/pdftk https://github.com/lob/lambda-pdftk-example/raw/master/bin/pdftk
-sudo chmod a+x /usr/bin/pdftk
+ARCHITECTURE=$(uname -m)
+if [ "$ARCHITECTURE" = "aarch64" ]; then
+	# For aarch64:
+	wget https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/p/pdftk-java-3.3.2-1.el7.noarch.rpm
+	sudo yum -y install apache-commons-lang3 bouncycastle
+	sudo rpm -Uvh pdftk-java*rpm
+else
+	sudo wget -O /usr/lib64/libgcj.so.10 https://github.com/lob/lambda-pdftk-example/raw/master/bin/libgcj.so.10
+	sudo wget -O /usr/bin/pdftk https://github.com/lob/lambda-pdftk-example/raw/master/bin/pdftk
+	sudo chmod a+x /usr/bin/pdftk
+fi
--- tools.sh
+++ tools.sh
@@ -6,7 +6,7 @@ sudo yum update
 sudo yum install -y zlib-devel pcre2-devel make gcc zlib-devel pcre2-devel sqlite-devel openssl-devel libevent-devel systemd-devel
 
 # from imagemagick.sh
-sudo yum makecache
+sudo yum -y makecache
 sudo yum -y install libtool
 sudo yum -y install gcc-c++
 sudo yum -y groupinstall "Development Tools"
